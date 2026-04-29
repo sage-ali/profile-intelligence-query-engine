01fa5d06b128bfdd22667a4b310509fca9a2a80a
sage-ali <81039794+sage-ali@users.noreply.github.com>
2026-04-29 15:17:36 +0100

    fix: ensure token reset on logout

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -172,6 +172,7 @@ export class AuthController {
 
     res.clearCookie('access_token');
     res.clearCookie('refresh_token');
+    res.clearCookie('csrf_token');
 
     return {
       status: 'success',
